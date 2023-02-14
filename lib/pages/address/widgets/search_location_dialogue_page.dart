389ad0cb9da036f8d6211fac796853c8f60f2d02
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2023-02-14 09:40:07 +0800

    resolved bug search address

--- lib/pages/address/widgets/search_location_dialogue_page.dart
+++ lib/pages/address/widgets/search_location_dialogue_page.dart
@@ -27,64 +27,66 @@ class LocationDialogue extends StatelessWidget {
         ),
         child: SizedBox(
             width: Dimensions.screenWidth,
-            child: TypeAheadField(
-              textFieldConfiguration: TextFieldConfiguration(
-                controller: _controller,
-                textInputAction: TextInputAction.search,
-                autofocus: true,
-                textCapitalization: TextCapitalization.words,
-                keyboardType: TextInputType.streetAddress,
-                decoration: InputDecoration(
-                  hintText: "search location",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide:
-                        const BorderSide(style: BorderStyle.none, width: 0),
+            child: SingleChildScrollView(
+              child: TypeAheadField(
+                textFieldConfiguration: TextFieldConfiguration(
+                  controller: _controller,
+                  textInputAction: TextInputAction.search,
+                  autofocus: true,
+                  textCapitalization: TextCapitalization.words,
+                  keyboardType: TextInputType.streetAddress,
+                  decoration: InputDecoration(
+                    hintText: "search location",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide:
+                          const BorderSide(style: BorderStyle.none, width: 0),
+                    ),
+                    hintStyle: Theme.of(context).textTheme.headline2?.copyWith(
+                        color: Theme.of(context).disabledColor,
+                        fontSize: Dimensions.font16),
                   ),
-                  hintStyle: Theme.of(context).textTheme.headline2?.copyWith(
-                      color: Theme.of(context).disabledColor,
-                      fontSize: Dimensions.font16),
                 ),
-              ),
-              onSuggestionSelected: (Prediction suggestion) {
-                Get.find<LocationController>().setLocation(
-                    suggestion.placeId!,
-                    suggestion.description!,
-                    mapController);
+                onSuggestionSelected: (Prediction suggestion) {
+                  Get.find<LocationController>().setLocation(
+                      suggestion.placeId!,
+                      suggestion.description!,
+                      mapController);
 
-                Get.back();
-              },
-              // as we type, it gives user suggestion
-              suggestionsCallback: (pattern) async {
-                return await Get.find<LocationController>()
-                    .searchLocation(context, pattern);
-              },
-              itemBuilder: (context, Prediction suggestion) {
-                return Padding(
-                  padding: EdgeInsets.all(Dimensions.width10),
-                  child: Row(
-                    children: [
-                      Icon(Icons.location_on),
-                      Expanded(
-                        child: Text(
-                          suggestion.description!,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline2
-                              ?.copyWith(
-                                  color: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1
-                                      ?.color,
-                                  fontSize: Dimensions.font16),
+                  Get.back();
+                },
+                // as we type, it gives user suggestion
+                suggestionsCallback: (pattern) async {
+                  return await Get.find<LocationController>()
+                      .searchLocation(context, pattern);
+                },
+                itemBuilder: (context, Prediction suggestion) {
+                  return Padding(
+                    padding: EdgeInsets.all(Dimensions.width10),
+                    child: Row(
+                      children: [
+                        Icon(Icons.location_on),
+                        Expanded(
+                          child: Text(
+                            suggestion.description!,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline2
+                                ?.copyWith(
+                                    color: Theme.of(context)
+                                        .textTheme
+                                        .bodyText1
+                                        ?.color,
+                                    fontSize: Dimensions.font16),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              },
+                      ],
+                    ),
+                  );
+                },
+              ),
             )),
       ),
     );
